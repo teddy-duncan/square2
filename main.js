af7d8822f8f362c021a533ba1a68c51c36142ccb
teddy-duncan <77077101+teddy-duncan@users.noreply.github.com>
2021-11-17 18:31:09 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+noseX=0;
+noseY=0;
+difference=0;
+leftwristX=0;
+rightwristX=0;
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("poseNet is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        leftwristX=results[0].pose.leftWrist.x;
+        rightwristX=results[0].pose.rightWrist.x;
+        difference=floor(leftwristX-rightwristX);
+        console.log("leftwristX="+leftwristX+"rightWristX="+rightwristX+"difference="+difference);
+    }
+}
+function draw(){
+    background("#969a97");
+    document.getElementById("square_side").innerHTML="width and height of a square will be"+difference+"px";
+    fill('#f90093');
+    stroke('#f90093');
+    square(noseX,noseY,difference);
+    
+}
